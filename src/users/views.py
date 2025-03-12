e8411b6e49b299e126470b926d008b02be714983
aiker1548 <aiker03@mail.ru>
2025-03-12 20:55:17 +0300

    feat: create route /token/

--- src/users/views.py
+++ src/users/views.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, status, HTTPException
 
-from src.users.schemas import UserCreate, UserOut
+from src.users.schemas import User, UserOut, UserResponseWithToken, UserToken
 from src.database.core import DbSession
 from src.users import crud
 
 router = APIRouter()
 
 
-@router.post("/users/", response_model=UserOut, status_code=status.HTTP_201_CREATED)
-async def register_user(db_session: DbSession, user: UserCreate):
+@router.post("/users/", response_model=UserResponseWithToken, status_code=status.HTTP_201_CREATED)
+async def register_user(db_session: DbSession, user: User):
     existing_user = await crud.get_user_by_username(db_session, user.username)
     if existing_user:
         raise HTTPException(
@@ -16,7 +16,8 @@ async def register_user(db_session: DbSession, user: UserCreate):
             detail="Username already registered"
         )
     created_user = await crud.create_user(db=db_session, username=user.username, email=user.email, password=user.password_hash)
-    return created_user
+    token = created_user.create_access_token()
+    return {"user": created_user, "access_token": token}
 
 @router.get("/users/{username}", response_model=UserOut, status_code=status.HTTP_200_OK)
 async def get_user_by_name(db_session: DbSession, username: str):
@@ -32,3 +33,20 @@ async def get_user_by_name(db_session: DbSession, username: str):
 async def get_users(db_session: DbSession):
     users = await crud.get_users(db_session)
     return users.scalars().all()
+
+
+@router.get("/users/token/", response_model=dict, status_code=status.HTTP_200_OK)
+async def get_token(db_session: DbSession, user_data: UserToken):
+    user = await crud.get_user_by_username(db_session, user_data.username)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User not found"
+        )
+    if not user.check_password(user_data.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid password"
+        )
+    token = user.create_access_token()
+    return token
