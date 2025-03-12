8f2653bac9fb6a49fb0b647d04af634bded29616
aiker1548 <aiker03@mail.ru>
2025-03-12 20:53:13 +0300

    feat: models for routers /token

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,10 +1,24 @@
-from pydantic import BaseModel, EmailStr
+import bcrypt
+import secrets
+import string
+
+from pydantic import BaseModel, EmailStr, validator
+
+
 
 # Модель для создания пользователя
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password_hash: str
+
+    @validator("email")
+    def email_required(cls, v):
+        if not v:
+            raise ValueError("Must not be empty string and must be a email")
+        return v
+    
+
 
 # Модель для представления пользователя в ответах
 class UserOut(BaseModel):
@@ -14,3 +28,12 @@ class UserOut(BaseModel):
 
     class Config:
         orm_mode = True
+
+class UserResponseWithToken(BaseModel):
+    user: UserOut
+    access_token: dict
+
+
+class UserToken(BaseModel):
+    username: str
+    password: str
