6277cb55299888a6e59787cf478b19d40ef4da22
aiker1548 <aiker03@mail.ru>
2025-03-13 22:42:30 +0300

    feat: include AuthMiddleware

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
-from src.middleware import DBSessionMiddleware  # Импортируй свою мидлвару
+from src.middleware import DBSessionMiddleware, AuthMiddleWare  # Импортируй свою мидлвару
 from src.users.views import router as user_router
 
 app = FastAPI()
 
 # Добавляем мидлвару в приложение
+app.add_middleware(AuthMiddleWare)
 app.add_middleware(DBSessionMiddleware)
 
 app.include_router(user_router)
