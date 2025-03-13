6fb65a1a73b9e843363a0d6e0278d236ca50c5f1
aiker1548 <aiker03@mail.ru>
2025-03-13 22:39:13 +0300

    fix: get_current_user. Get user from request.state

--- src/users/crud.py
+++ src/users/crud.py
@@ -1,9 +1,18 @@
+from typing import Annotated
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.exc import SQLAlchemyError
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import HTTPException, status, Depends
+from starlette.requests import Request
 
 from src.database.models import User
 from src.users.utils import hash_password
+from src.auth import verify_access_token
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/users/token')
 
 async def create_user(db: AsyncSession, username: str, email: str, password: str):
     password_hash = hash_password(password)
@@ -24,3 +33,24 @@ async def get_user_by_username(db: AsyncSession, username: str):
 async def get_users(db: AsyncSession) -> list[User]:
     result = await db.execute(select(User))
     return result
+
+
+# async def get_current_user(db: AsyncSession, token: str = Depends(oauth2_scheme)): 
+#     payload = verify_access_token(token)
+#     if payload is None:
+#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
+#     user = await get_user_by_username(db, payload.get('sub'))
+#     if not user:
+#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User not found')
+#     return user
+
+def get_current_user(request: Request) -> User:
+    user = getattr(request.state, "user", None)  # Избегаем AttributeError
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Authentication required"
+        )
+    return user
+
+Current_user = Annotated[User, Depends(get_current_user)]
